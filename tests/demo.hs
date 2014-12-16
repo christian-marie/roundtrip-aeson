932c08535e324368f6202a58cdc071ead01efee9
Thomas Sutton <me@thomas-sutton.id.au>
2014-12-16 13:50:41 +1100

    Clean up

--- tests/demo.hs
+++ tests/demo.hs
@@ -6,6 +6,10 @@ import Control.Isomorphism.Partial
 import Data.Aeson
 import Data.Aeson.Lens
 import qualified Data.ByteString.Lazy.Char8 as L
+import Data.Monoid
+import Data.Text (Text)
+import Data.Vector (Vector)
+import qualified Data.Vector as V
 import Text.Roundtrip.Classes
 
 import Data.Aeson.RoundTrip
@@ -19,22 +23,49 @@ data Invoice
 
 defineIsomorphisms ''Invoice
 
+data Account = Account Text [Invoice]
+  deriving Show
+
+defineIsomorphisms ''Account
+
+isoListVector :: Iso (Vector v) [v]
+isoListVector = unsafeMakeIso (Just . V.toList) (Just . V.fromList)
+
 invoiceSyntax :: JsonSyntax s => s Invoice
 invoiceSyntax =
     unpaid
-        <$> jsonField "foo" (demote _Bool <$> value)
-        <*> jsonField "bar" (demote _Integer <$> value)
-        <*> jsonField "baz" (jsonArray (demote _Bool <$> value))
+        <$> jsonField "foo" jsonBool
+        <*> jsonField "bar" (demote _Integer <$> jsonNumber)
+        <*> jsonField "baz" (isoListVector <$> jsonArray (demote _Bool <$> value))
     <|> paid
         <$> jsonField "bar" (demote _Integer <$> value)
 
+accountSyntax :: JsonSyntax s => s Account
+accountSyntax = account
+    <$> jsonField "name" jsonString
+    <*> jsonField "invoices" (isoListVector <$> jsonArray invoiceSyntax)
+
 main :: IO ()
 main = do
-    putStrLn "UNPARSE"
+    putStrLn "FIELDS"
+    putStrLn "\tUNPARSE"
     let Just x = runBuilder invoiceSyntax $ Unpaid False 40 [False]
     let Just y = runBuilder invoiceSyntax $ Paid 42
-    L.putStrLn $ encode x
-    L.putStrLn $ encode y
-    putStrLn "PARSE"
-    print (runParser invoiceSyntax x)
-    print (runParser invoiceSyntax y)
+    L.putStrLn $ "\t" <> encode x
+    L.putStrLn $ "\t" <> encode y
+    putStrLn "\n\tPARSE"
+    putStrLn $ "\t" <> show (runParser invoiceSyntax x)
+    putStrLn $ "\t" <> show (runParser invoiceSyntax y)
+
+    putStrLn "\n\nLISTS"
+    putStrLn "\tUNPARSE"
+    let Just z1 = runBuilder accountSyntax $ Account "Foo"
+            [ Unpaid False 40 [False]
+            , Paid 42
+            ]
+    L.putStrLn $ "\t" <> encode z1
+    let Just z2 = runBuilder accountSyntax $ Account "Bar" []
+    L.putStrLn $ "\t" <> encode z2
+    putStrLn "\n\tPARSE"
+    putStrLn $ "\t" <> show (runParser accountSyntax z1)
+    putStrLn $ "\t" <> show (runParser accountSyntax z2)
